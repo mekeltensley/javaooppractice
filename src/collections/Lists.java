f183754a8d54be7753044223f73983651f43ecc3
Mekel Tensley <mekelintech@gmail.com>
2023-01-21 02:39:51 -0500

    Add new List with methods

---
+++ src/collections/Lists.java
@@ -0,0 +1,30 @@
+package collections;
+
+import com.sun.source.tree.LiteralTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+
+    public static void main(String[] args) {
+
+        List fruits = new ArrayList();
+
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("banana");
+        fruits.add("orange");
+        fruits.set(2, "grape");
+        fruits.add("lemon");
+        fruits.remove(4);
+
+
+        System.out.println(fruits.get(1));
+        System.out.println(fruits.get(2));
+        System.out.println("index of first lemon: " + fruits.indexOf("lemon"));
+        System.out.println("index of last lemon " + fruits.lastIndexOf("lemon"));
+
+        List moreFruit = List.of("cherry", "cranberry", "plum");
+    }
+}
